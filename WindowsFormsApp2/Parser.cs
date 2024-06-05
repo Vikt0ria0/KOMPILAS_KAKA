a11afb05f1b8fb9002930d1b9c66afa91023fe8e
Vikt0ria0 <91361993+Vikt0ria0@users.noreply.github.com>
2024-06-06 00:32:52 +0700

    Update Parser.cs

--- WindowsFormsApp2/Parser.cs
+++ WindowsFormsApp2/Parser.cs
@@ -90,14 +90,57 @@ namespace WindowsFormsApp2
                 StateState stateLex = magasin.state[0];
                 magasin.state.Remove(0);
 
-                //
+                //Type typeLex = magasin.type[0];
+                //magasin.type.RemoveAt(0);
             }
         }
 
-        //public void Pair(State stateLex)
-        //{
-        //      if(stateLex.state[0] == F && stateLex.type[0] == NAME)
-        //
-        //}
+        public void Pair(State stateLex)
+        {
+              if(stateLex.state[0] == F && stateLex.type[0] == NAME)
+            //if (stateLex == StateState.F && typeLex == Type.NAME){
+            //      пара F и Имя 
+            //      Console.WriteLine("Найдена пара F и NAME");
+        }
     }
+
+/*            public enum Type
+        {
+            None = 0,
+            INTEGER,
+            FLOAT,
+            PLUS,
+            MINUS,
+            MULTIPLY,
+            DIVIDE,
+            LPAREN,
+            RPAREN,
+            SEMICOLON,
+            COMMA,
+            LBRACE,
+            RBRACE,
+            LSQUARE,
+            RSQUARE,
+            NOT,
+            LESS,
+            GREATER,
+            LESS_OR_EQUAL,
+            GREATER_OR_EQUAL,
+            EQUAL,
+            NOT_EQUALS,
+            ASSIGN,
+            INT_DECLARE,
+            FLOAT_DECLARE,
+            ARRAY_DECLARE,
+            READ,
+            WRITE,
+            IF,
+            ELSE,
+            WHILE,
+            AND,
+            OR,
+            NAME,
+            EOL,
+            END
+        }; */
 }
